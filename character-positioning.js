aa0ac94dc00fe4b864b07037a2bf48e86b78c63f
Sherlaine <33073955+Sherlaine@users.noreply.github.com>
2018-09-26 15:40:11 -0700

    Add files via upload

---
+++ character-positioning.js
@@ -0,0 +1,23 @@
+function characterPosition(string) {
+    // add empty array for each character that logs it's index
+ 
+    var noSpaceString = string.split(" ").join('');
+    var letterCounts = {};
+ 
+    for (var i = 0; i < noSpaceString.length; i++){
+ 
+        if (!letterCounts[noSpaceString[i]]) {
+            // add empty array for i current position starting at 0
+            letterCounts[noSpaceString[i]] = [i];
+        } else {
+            // push aditional postion into array if more than
+            // one instance of character
+            letterCounts[noSpaceString[i]].push(i);
+        }
+    }
+ 
+    console.log(letterCounts);
+ 
+ };
+ 
+ characterPosition("lighthouse in the house");
